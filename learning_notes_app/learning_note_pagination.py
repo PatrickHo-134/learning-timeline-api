e2417b49a4188e64f0c0b8b33abb476b9cd76da0
Patrick Ho <patrick@blueridgeglobal.com.au>
2024-10-15 00:49:16 +1100

    #9 Update page size to 10

--- learning_notes_app/learning_note_pagination.py
+++ learning_notes_app/learning_note_pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class LearningNotePagination(PageNumberPagination):
-    page_size = 5  # Number of learning notes per page
+    page_size = 10
     page_size_query_param = 'page_size'  # Allow overriding the default page size
     max_page_size = 100
 
